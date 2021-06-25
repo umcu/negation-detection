6e0ddfb44bf0c61fbb15073d4963eb00271bfb62
Bram <bramiozo@gmail.com>
2021-06-25 11:37:36 +0200

    python3 extract_ntvg.py --out_folder bla

--- sandbox/extract_ntvg.py
+++ sandbox/extract_ntvg.py
@@ -6,10 +6,13 @@ import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import argparse
+from getpass import getpass
+from tqdm import tqdm
+from pathlib import Path
 
 def textify(raw_text):
     if (raw_text is not None) and (isinstance(raw_text, str)):
-        soup = BeautifulSoup(raw_text)
+        soup = BeautifulSoup(raw_text, features='lxml')
         txt = soup.getText()
         txt = re.sub(r'\[\[.*\]\]','', txt)
         txt = re.sub(r'\s{2,}',' ', txt)
@@ -21,8 +24,8 @@ def textify(raw_text):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Processing input for the NtvG extractor')
     parser.add_argument('--in', dest='input_file', type=str, default=None)
-    parser.add_argument('--out_folder', dest='output_folder', type=str)
-    parser.add_argument('--source', dest='source', help='Cleaning settings', type=str)
+    parser.add_argument('--out_folder', dest='output_folder', type=str, default="./out")
+    parser.add_argument('--source', dest='source', help='Cleaning settings', type=str, default="sql")
     args = parser.parse_args()
 
     #################
@@ -30,16 +33,18 @@ if __name__ == "__main__":
     #################
     # from csv
     if args.source == 'file':
+        assert args.input_file is not None, "Input error: you need to provide the path to the input file"
         df = pd.read_csv(args.input_file,
                             sep=",", 
                             names=['id', 'body', 'samenvatting', 'article_type', 'article_category'])
     # from sql
     if args.source == 'sql':
+        load_dotenv()
         user = os.getenv('MYSQL_USER')
-        password = os.getenv('MYSQL_PASSWORD')
         host = os.getenv('MYSQL_HOST')
         port = os.getenv('MYSQL_PORT')
         database = os.getenv('MYSQL_DATABASE')
+        password= getpass(prompt="Please provide SQL pass: ")
 
         # Create the connection
         connection_string = f'mysql://{user}:{password}@{host}:{port}/{database}'
@@ -83,21 +88,28 @@ if __name__ == "__main__":
     ## WRITE ########
     #################
     # to feather 
+    Path(args.output_folder).mkdir(parents=True, exist_ok=True)
     df[['id', 'body_txt', 'samenvatting_txt', 'article_type', 'article_category']]\
-            .to_feather(os.path.join(args.output_folder, "ntvg_articles.feather")
+            .to_feather(os.path.join(args.output_folder, "ntvg_articles.feather"))
 
     # to individual files per category
+    new_folder = os.path.join(args.output_folder, 'category')
+    Path(new_folder).mkdir(parents=True, exist_ok=True)
     for _cat in df.article_category.unique():
-        dfc = df[df.article_category==_cat]
-        dfc.fillna('')
-        with open(os.path.join(args.output_folder, 'category', _cat, ".txt"), 'w', encoding='utf-8') as fw:
-            for txt in dfc[['body_txt', 'samenvatting_txt']].sum(axis=1):
-                fw.write(txt+"\n\r")
+        if _cat is not None:
+            dfc = df[df.article_category==_cat]
+            dfc.fillna('', inplace=True)
+            with open(os.path.join(new_folder, re.sub(r"[^\w]","_", _cat)+".txt"), 'w', encoding='utf-8') as fw:
+                for txt in tqdm(dfc[['body_txt', 'samenvatting_txt']].sum(axis=1)):
+                    fw.write(txt+"\n")
 
     # to individual files per article type
+    new_folder = os.path.join(args.output_folder, 'article_type')
+    Path(new_folder).mkdir(parents=True, exist_ok=True)
     for _type in df.article_type.unique():
-        dfc = df[df.article_type==_type]
-        dfc.fillna('')
-        with open(os.path.join(args.output_folder, 'article_type', _type, ".txt", encoding='utf-8'), 'w') as fw:
-            for txt in dfc[['body_txt', 'samenvatting_txt']].sum(axis=1):
-                fw.write(txt+"\n\r")
+        if _type is not None:
+            dfc = df[df.article_type==_type]
+            dfc.fillna('', inplace=True)
+            with open(os.path.join(new_folder, re.sub(r"[^\w]","_", _type)+".txt"), 'w', encoding='utf-8') as fw:
+                for txt in tqdm(dfc[['body_txt', 'samenvatting_txt']].sum(axis=1)):
+                    fw.write(txt+"\n")
