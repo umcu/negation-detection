49f3b8c7ae7ed15903ed39ad423c19dcf84f1d10
Bram van Es <bramiozo@gmail.com>
2023-12-12 14:19:26 +0100

    Auto stash before merge of "bram_error_types" and "origin/bram_error_types"

---
+++ sandbox/koekiemonster_torch_fix.sh
@@ -0,0 +1,2 @@
+#!source
+export LD_LIBRARY_PATH=/media/koekiemonster/home/bramiozo/.pyenvv/versionv/vv/versions/3.v/versions/3.10.v/versions/3.v/versiov/vvv/vv/versions/3.10.4/envs/nlp/lib/python3.10/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
